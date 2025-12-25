f639232ad2eb610ee55d42121a330489d51c98a5
MadTitan <89219545+RomanNihal@users.noreply.github.com>
2025-12-25 11:48:26 +0600

    v1

--- DVC/main.py
+++ DVC/main.py
@@ -20,3 +20,20 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 df.to_csv(file_path, index=False)
 
 print(f"CSV file saved to {file_path}")
+
+# pip install dvc
+# Now we do "dvc init" (creates .dvcignore, .dvc)
+# Now do "mkdir DVC/S3" (creates a new S3 directory)
+# Now we do "dvc remote add -d s3remote DVC/S3"
+# Next "dvc add DVC/data/" 
+# Now it will ask to do: ("git rm -r --cached 'DVC/data'" and "git commit -m "stop tracking DVC\data"")
+# Because initially we were tracking data/ folder from git so now we remove it for DVC to handle.
+
+# Again we do "dvc add DVC/data/" (creates DVC/data.dvc)
+# Now - "dvc commit" and then "dvc push"
+# Do a git add-commit-push to mark this stage as first version of data.
+
+# Now make changes to main.py to append a new row in data, check changes via "dvc status"
+# Again - - "dvc commit" and then "dvc push"
+# Then git add-commit-push (we're saving V2 of our data at this point)
+# Check dvc/git status, everything should be upto date.
